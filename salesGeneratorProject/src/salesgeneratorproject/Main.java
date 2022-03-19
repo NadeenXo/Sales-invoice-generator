e7432cb9c559a6fe1e16f399895d336215836ad2
Nadeen <75531472+NadeenXo@users.noreply.github.com>
2022-03-19 16:40:36 +0200

    first submition

---
+++ salesGeneratorProject/src/salesgeneratorproject/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salesgeneratorproject;
+
+import SalesGeneratorProject.SalesGeneratorProject;
+
+/**
+ *
+ * @author nadeen
+ */
+public class Main {
+    public static void main(String args[]) {
+
+        SalesGeneratorProject frame = new SalesGeneratorProject();
+        frame.setTitle("sales generator");
+        frame.setVisible(true);
+    }
+}
